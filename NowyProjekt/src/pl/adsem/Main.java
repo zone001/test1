d5e85e7b02586b3edd45b24751b4b5e6b329af5a
Marcin J <marcinj.@Marcins-MacBook-Pro.local>
2021-11-08 18:37:26 +0100

    Nowy projekt3

---
+++ NowyProjekt/src/pl/adsem/Main.java
@@ -0,0 +1,11 @@
+package pl.adsem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+        System.out.println("hello world2");
+
+    }
+}
